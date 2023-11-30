2b30710d371563bd7ffded006f0382eac79b56be
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-11-30 10:15:57 -0500

    Make fish restless

--- fish.gd
+++ fish.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var speed = 400
+@export var move_radius = 400
 
 func move_to_pos(pos):
 	var direction = global_position.direction_to(pos)
@@ -8,3 +9,11 @@ func move_to_pos(pos):
 	while global_position.distance_to(pos) > 10:
 		position += speed * direction * get_physics_process_delta_time()
 		await get_tree().physics_frame
+
+
+func _on_vision_radius_area_entered(area):
+	if area != self:
+		print("Fish sees ", area.name)
+
+func _on_move_timer_timeout():
+	move_to_pos(global_position + Vector2(randi_range(-move_radius, move_radius), randi_range(-move_radius, move_radius)))

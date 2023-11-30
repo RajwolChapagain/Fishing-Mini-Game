3273daad2a3af85d3a10f7b4d6d38aad02ef61c6
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-11-30 09:44:05 -0500

    Throw fishing line only to left and below water level

--- fisherman.gd
+++ fisherman.gd
@@ -1,8 +1,11 @@
 extends Sprite2D
 
+var water_level = global_position.y
+
 func _input(event):
 	if event.is_action_pressed("throw_line"):
-		throw_fishing_line()
+		throw_fishing_line(get_global_mouse_position())
 	
-func throw_fishing_line():
-	print("fishing line thrown")
+func throw_fishing_line(pos):
+	if pos.x < $Marker2D.global_position.x and pos.y > water_level:
+		print("fishing line thrown")

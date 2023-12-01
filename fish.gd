8ba712844f19ad4fc351f1fa991b1395985cef37
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-12-01 16:19:23 -0500

    Change movement code for fish
    Also add water and shore

--- fish.gd
+++ fish.gd
@@ -3,18 +3,21 @@ extends Area2D
 @export var speed = 400
 @export var move_radius = 400
 
-func move_to_pos(pos):
-	var direction = global_position.direction_to(pos)
-	
-	while global_position.distance_to(pos) > 10:
-		position += speed * direction * get_physics_process_delta_time()
-		await get_tree().physics_frame
+@onready var target = global_position
 
+func _physics_process(_delta):
+	move_to_target()
+	
+func move_to_target():
+	var direction = global_position.direction_to(target)
+	
+	if global_position.distance_to(target) > 10:
+		position += speed * direction * get_physics_process_delta_time()
 
 func _on_vision_radius_area_entered(area):
 	if area.name == "Bobber":
 		print("Fish sees Bobber")
-		move_to_pos(area.global_position)
+		target = area.global_position
 
 func _on_move_timer_timeout():
-	move_to_pos(global_position + Vector2(randi_range(-move_radius, move_radius), randi_range(-move_radius, move_radius)))
+	target += Vector2(randi_range(-move_radius, move_radius), randi_range(-move_radius, move_radius))

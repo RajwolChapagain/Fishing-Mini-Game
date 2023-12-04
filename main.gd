0b78bb3438abe3ce9594c7058bd81f630d9437ba
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-12-04 15:25:50 -0500

    Instantiate level through code

--- main.gd
+++ main.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+@export var levels : Array[PackedScene]
+
+func _ready():
+	instantitate_level_and_connect_signals(1)
+	pass
+	
+func _input(event):
+	if event.is_action_pressed("fail_level"):
+		_on_level_failed()
+		
 func _on_level_cleared(level):
 	if level == 1:
 		$Panels.show_level_cleared_panel()
@@ -18,8 +28,18 @@ func _on_panels_quit_button_pressed():
 
 
 func _on_panels_restart_button_pressed():
-	print("Restart the level")
-
+	for child in get_children():
+		if child.is_in_group("level"):
+			child.queue_free()
+			instantitate_level_and_connect_signals(child.level)
 
 func _on_panels_yay_button_pressed():
 	pass # Replace with function body.
+
+func instantitate_level_and_connect_signals(level):
+	var current_level = levels[level-1].instantiate()
+	add_child(current_level)
+	current_level.level_cleared.connect(_on_level_cleared)
+	current_level.level_failed.connect(_on_level_failed)
+	move_child(current_level, 0)
+	

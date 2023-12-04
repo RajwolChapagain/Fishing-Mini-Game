624c7faad5f32849afdb73615cf42bc712377633
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-12-04 14:46:10 -0500

    Create panels scene

--- main.gd
+++ main.gd
@@ -2,18 +2,9 @@ extends Node2D
 
 func _on_level_cleared(level):
 	if level == 1:
-		show_next_level_panel()
+		$Panels.show_level_cleared_panel()
 	elif level == 2:
-		show_congratulations_panel()
+		$Panels.show_congratulations_panel()
 
 func _on_level_failed():
-	show_retry_panel()
-	
-func show_next_level_panel():
-	print("Proceed to next level")
-
-func show_congratulations_panel():
-	print("You won the game!")
-
-func show_retry_panel():
-	print("Level failed! Retry")
+	$Panels.show_level_failed_panel()
---
+++ ui/panels.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func show_level_cleared_panel():
+	$LevelClearedPanel.visible = true
+	
+func show_level_failed_panel():
+	$LevelFailedPanel.visible = true
+
+func show_congratulations_panel():
+	$CongratulationsPanel.visible = true
